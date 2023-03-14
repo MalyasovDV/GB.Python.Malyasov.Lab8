6d0b3faf1b8d50e9b547692e27660b17ba172c81
Dmitry_Malyasov <dmalyasov24@gmail.com>
2023-03-14 17:32:22 +0700

    program done

---
+++ main.py
@@ -0,0 +1,27 @@
+from function import *
+
+try:
+    phone_book = load()
+except:
+    phone_book = {
+        "иванов иван иванович": {"mobilephone": ["79370757270"], "workphone": ["79370756666"], "birthday": ["13-08-1984"], "email": ["ivan_v@yandex.ru"]},
+        "петров петр петрович": {"mobilephone": ["79161100000"] , "workphone": ["79161122222"], "birthday": ["01-01-1990"], "email": ["petr_p@yandex.ru"]}}
+    save(phone_book)
+    print("Не удалось загрузить телефонную книгу. Создана тестовая телефонная книга.\n")
+
+choice = None
+while choice != 0:
+    menu()
+    choice = int(input("Введите пункт меню (цифру): "))
+    match choice:
+        case 0: print('\nВы вышли из программы. До свидания')
+        case 1: save(phone_book)
+        case 2: load()
+        case 3: print_all_data_contact()            
+        case 4: print_phones_contact()
+        case 5: print_all_fullname()
+        case 6: create_new_contant()
+        case 7: delete_contact()
+        case 8: edit_contact()
+        case 9: add_data()
+        case _: print("\nВыберите пункт меню или выйдите из программы (Цифра: 0)\n")
